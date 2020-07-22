cb23335c77723ff15f60ba30931b67e15c5f1c19
emtalen <59577366+emtalen@users.noreply.github.com>
2020-07-22 15:37:25 +0200

    fixing basic structure and adding to simple cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyHomePage());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -10,6 +18,20 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Expense Planner'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Container(width: 100, child: Text('CHART!')),
+            ),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
+        ],
       ),
     );
   }
