e1328ad94a73bad2b832f0171872a7e0de6994f2
emtalen <59577366+emtalen@users.noreply.github.com>
2020-07-22 15:05:01 +0200

    initial commit with scaffold

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyHomePage());
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Planner'),
+      ),
+    );
+  }
+}
