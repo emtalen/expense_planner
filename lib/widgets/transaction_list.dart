43330e4bbd24d42c7239f420fa2950b2745defa2
emtalen <59577366+emtalen@users.noreply.github.com>
2020-08-11 19:28:39 +0200

    making the decimals in the amout stay to two

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -28,7 +28,7 @@ class TransactionList extends StatelessWidget {
                   ),
                   padding: EdgeInsets.all(10),
                   child: Text(
-                    '${transactions[index].amount} kr',
+                    '${transactions[index].amount.toStringAsFixed(2)} kr',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 15,
