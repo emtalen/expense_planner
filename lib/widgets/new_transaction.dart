65718ce9f1c6bad84ea830b3c0e32c285674ba6c
emtalen <59577366+emtalen@users.noreply.github.com>
2020-08-14 10:47:09 +0200

    adding buttons to add a datepicker for when adding a new transaction

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -48,10 +48,29 @@ class _NewTransactionState extends State<NewTransaction> {
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               onSubmitted: (_) => submitData(),
             ),
-            FlatButton(
+            Container(
+              height: 70,
+              child: Row(
+                children: [
+                  Text('No Date Chosen!'),
+                  FlatButton(
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    onPressed: null,
+                  )
+                ],
+              ),
+            ),
+            RaisedButton(
               onPressed: submitData,
               child: Text('Add Transaction'),
-              textColor: Colors.purple,
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
             )
           ],
         ),
