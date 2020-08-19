962f59d2b0ef34b671fb4d3efe55d9e84f3772a4
emtalen <59577366+emtalen@users.noreply.github.com>
2020-08-19 13:31:08 +0200

    adding a switch so you can switch between the chart and the list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,63 +10,67 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  'No transactions added yet!',
-                  style: Theme.of(context).textTheme.title,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                    height: 200,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    )),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(
-                            '${transactions[index].amount.toStringAsFixed(2)} kr',
-                          ),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Container(
+          child: transactions.isEmpty
+              ? Column(
+                  children: [
+                    Text(
+                      'No transactions added yet!',
                       style: Theme.of(context).textTheme.title,
                     ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: TextStyle(color: Colors.grey),
+                    SizedBox(
+                      height: 20,
                     ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTrans(transactions[index].id),
-                    ),
-                  ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
+                    Container(
+                        height: constraints.maxHeight * 0.6,
+                        child: Image.asset(
+                          'assets/images/waiting.png',
+                          fit: BoxFit.cover,
+                        )),
+                  ],
+                )
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 5,
+                      margin: EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 5,
+                      ),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: FittedBox(
+                              child: Text(
+                                '${transactions[index].amount.toStringAsFixed(2)} kr',
+                              ),
+                            ),
+                          ),
+                        ),
+                        title: Text(
+                          transactions[index].title,
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                        subtitle: Text(
+                          DateFormat.yMMMd().format(transactions[index].date),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTrans(transactions[index].id),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: transactions.length,
+                ),
+        );
+      },
     );
   }
 }
